c29ebecfcbb5454d75a0d74e36900ed71a3b5161
LucBen ASEM <l.bennati@asem.it>
2022-12-08 15:32:43 +0100

    version1

---
+++ ProjectFiles/NetSolution/ClockLogic.cs
@@ -0,0 +1,33 @@
+#region Using directives
+using System;
+using CoreBase = FTOptix.CoreBase;
+using FTOptix.HMIProject;
+using UAManagedCore;
+using FTOptix.UI;
+using FTOptix.NetLogic;
+using FTOptix.EventLogger;
+using FTOptix.Alarm;
+#endregion
+
+public class ClockLogic : BaseNetLogic
+{
+	public override void Start()
+	{
+		periodicTask = new PeriodicTask(UpdateTime, 1000, LogicObject);
+		periodicTask.Start();
+	}
+
+	public override void Stop()
+	{
+		periodicTask.Dispose();
+		periodicTask = null;
+	}
+
+	private void UpdateTime()
+	{
+		LogicObject.GetVariable("Time").Value = DateTime.Now;
+		LogicObject.GetVariable("UTCTime").Value = DateTime.UtcNow;
+	}
+
+	private PeriodicTask periodicTask;
+}
